8356461c40db511511f6c5778c54b9b69d261b22
LoicHijl <loic_hijl@hotmail.com>
2026-05-28 16:45:36 +0200

    Create ingest_matches.py
    Create ingest_matches.py
    Made to test the API functionality and get familiar with it.
    fetch_tournament_data() takes in a tournament name and spits out matches played in this tournament.
    
    save_raw_data() takes in data (from the fetch) and a filename and saves the data into the filename in a specified path.
    
    The main function is currently set to run on Worlds 2025.

---
+++ ingest_matches.py
@@ -0,0 +1,49 @@
+import json
+import os
+from mwrogue.esports_client import EsportsClient
+from mwrogue.auth_credentials import AuthCredentials
+
+credentials = AuthCredentials(user_file = "me")
+
+def fetch_tournament_data(tournament_name):
+    # Connect to the Leaguepedia MediaWiki instance
+    site = EsportsClient("lol", credentials=credentials)
+
+    print(f"Connecting to Leaguepedia to fetch data for: {tournament_name}...")
+
+    # Query the Cargo database
+    response = site.cargo_client.query(
+        tables = "ScoreboardGames=SG, Tournaments=T",
+        join_on = "SG.OverviewPage=T.OverviewPage",
+        fields = ", SG.DateTime_UTC, SG.Team1, " \
+        "SG.Team2, SG.WinTeam, SG.Gamelength, " \
+        "SG.Team1Bans, SG.Team2Bans, SG.Team1Picks, " \
+        "SG.Team2Picks, SG.Team1Gold, SG.Team2Gold",
+        where = f"SG.OverviewPage LIKE '%{tournament_name}%'",
+        order_by = "SG.DateTime_UTC"
+    )
+
+    return response
+
+def save_raw_data(data, filename="raw_msi_games.json"):
+    os.makedirs("data/raw", exist_ok=True)
+    filepath = os.path.join("data/raw", filename)
+
+    with open(filepath, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+
+    print(f"Successfully saved {len(data)} games to {filepath}.")
+
+if __name__ == "__main__":
+    target_tournament = "2025 Season World Championship"
+
+    try:
+        raw_games = fetch_tournament_data(target_tournament)
+        if raw_games:
+            save_raw_data(raw_games)
+        else:
+            print("No games found")
+    except Exception as e:
+        print(f"An error occured: {e}")
+
+    
