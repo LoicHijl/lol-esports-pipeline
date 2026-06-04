b208e23dbf3afd81c30358b5eda545ab6f6c2159
LoicHijl <loic_hijl@hotmail.com>
2026-06-04 16:20:32 +0200

    Refactor fetching
    Refactor fetching to make it more universal. Now the fetch_tournament_data is a dedicated function to fetch the test data. Later on this will also be removed.

--- ingest_matches.py
+++ ingest_matches.py
@@ -23,27 +23,52 @@ if not all([WIKI_PASS, WIKI_USER, AWS_KEY, AWS_SECRET,AWS_REGION]):
     print("ERROR: Missing environment variables!")
     sys.exit(1)
 
-def fetch_tournament_data(tournament_name):
-    # Connect to the Leaguepedia MediaWiki instance
-    site = EsportsClient("lol", credentials=credentials)
-
-    print(f"Connecting to Leaguepedia to fetch data for: {tournament_name}...")
+def fetch_tournament_data(tournament_name, site = None):
+    # Fallback condition if no site instance was passed
+    if site is None:
+        print("No active session passed in. Creating a new EsportsClient connection.....")
+        # Connect to the Leaguepedia MediaWiki instance
+        site = EsportsClient("lol", credentials=credentials)
 
     # Query the Cargo database
-    response = site.cargo_client.query(
+    response = fetch_cargo_data(
         tables = "ScoreboardGames=SG, Tournaments=T",
-        join_on = "SG.OverviewPage=T.OverviewPage",
         fields = ", SG.DateTime_UTC, SG.Team1, " \
         "SG.Team2, SG.WinTeam, SG.Gamelength, " \
         "SG.Team1Bans, SG.Team2Bans, SG.Team1Picks, " \
         "SG.Team2Picks, SG.Team1Gold, SG.Team2Gold",
-        where = f"SG.OverviewPage LIKE '%{tournament_name}%'",
+        condition = f"SG.OverviewPage LIKE '%{tournament_name}%'",
+        site=site,
+        join_on = "SG.OverviewPage=T.OverviewPage",
         order_by = "SG.DateTime_UTC"
     )
 
     return response
 
-def save_raw_data(data, filename="raw_msi_games.json"):
+def fetch_cargo_data(tables, fields, condition = None, site = None, **kwargs):
+    """Fetch any type of data from the cargo tables of the MediaWiki site instance"""
+    # Fallback condition if no site instance was passed
+    if site is None:
+        print("No active session passed in. Creating a new EsportsClient connection...")
+        # Connect to the Leaguepedia MediaWiki instance
+        site = EsportsClient("lol", credentials=credentials)
+
+    if condition is None:
+        print(f"Fetching [{fields}] from [{tables}]")
+    else:
+        print(f"Fetching [{fields}] from [{tables}] with condition: {condition}")
+
+    # Get the response from MediaWiki API
+    response = site.cargo_client.query(
+        tables=tables,
+        fields=fields,
+        where=condition,
+        **kwargs
+    )
+
+    return response
+
+def save_raw_data(data, filename = "raw_msi_games.json"):
     os.makedirs("data/raw", exist_ok=True)
     filepath = os.path.join("data/raw", filename)
 
@@ -79,7 +104,7 @@ if __name__ == "__main__":
         raw_games = fetch_tournament_data(target_tournament)
         if raw_games:
             save_raw_data(raw_games)
-            upload_to_s3(raw_games)
+            # upload_to_s3(raw_games)
         else:
             print("No games found")
     except Exception as e:
